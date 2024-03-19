06de32866eb0b038f5bc00f3d6fc157ebd4adc1f
johnny-fernandez <145487736+johnny-fernandez@users.noreply.github.com>
2024-03-19 13:12:44 -0400

    problem 1 finished

---
+++ Exercise10/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+            while (choice != 4)
+            {
+
+                //Print menu
+                Console.WriteLine("**************");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1 - New Game");
+                Console.WriteLine("2 - Load Game");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Quit");
+                Console.WriteLine("**************");
+                Console.WriteLine();
+
+                //prompt for and get user choice
+                Console.WriteLine("Please enter 1, 2, 3, or 4");
+                choice = int.Parse(Console.ReadLine());
+                Console.WriteLine(choice);
+            }
+        
+        } 
+    }
+}
